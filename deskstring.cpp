fb3be220c69c830a545ad27fbea73d785458d026
Tarikhoza <tarik.246@hotmail.com>
2018-02-01 15:42:09 +0100

    Add files via upload

---
+++ deskstring.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void LOG(string input) {
+	fstream LogFile;
+	LogFile.open("dat.txt", fstream::app);
+	if (LogFile.is_open()) {
+		LogFile << input;
+		LogFile.close();
+	}
+}
+
+
+
+int main()
+{
+	string output;
+    cin>>output;
+	
+					fstream LogFile;
+					LogFile.open("dat.txt", fstream::app);
+					if (LogFile.is_open()) {
+						LogFile << string(output);
+						LogFile.close();
+					}
+
+				
+
+	return 0;
+}
+
