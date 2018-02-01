7eb2f4b6ff94ca2d5cce9fbf0f969216e4c47c8f
Tarikhoza <tarik.246@hotmail.com>
2018-02-01 16:16:37 +0100

    Update deskstring.cpp

--- deskstring.cpp
+++ deskstring.cpp
@@ -3,22 +3,22 @@
 #include <fstream>
 using namespace std;
 void LOG(string input) {
-	fstream LogFile;
-	LogFile.open("dat.txt", fstream::app);
-	if (LogFile.is_open()) {
-		LogFile << input;
-		LogFile.close();
+fstream LogFile;
+LogFile.open("dat.txt", fstream::app);
+if (LogFile.is_open()) {
+LogFile << input;
+LogFile.close();
 	}
 }
 int main()
 {
-	string output;
-    cin>>output;
-					fstream LogFile;
-					LogFile.open("log.txt", fstream::app);
-					if (LogFile.is_open()) {
-						LogFile << string(output);
-						LogFile.close();
+string output;
+cin>>output;
+fstream LogFile;
+LogFile.open("log.txt", fstream::app);
+if (LogFile.is_open()) {
+LogFile << string(output);
+LogFile.close();
 					}
 }
 
